223ee411b5c18321c7fc814a688f3fc14fcc7ffa
Fameless <97400398+Anshul-sys32@users.noreply.github.com>
2026-04-02 08:24:58 +0530

    Linked List Custody Implementation
    Added CustodyNode and CustodyChain classes to manage evidence custody records.

--- DECoS.cpp
+++ DECoS.cpp
@@ -23,3 +23,50 @@ struct Evidence {
 };
 
 unordered_map<string, Evidence> evidenceTable;
+
+struct CustodyNode {
+    string action;
+    string user;
+    string timestamp;
+    CustodyNode* next;
+
+    CustodyNode(string a, string u, string t) {
+        action = a;
+        user = u;
+        timestamp = t;
+        next = NULL;
+    }
+};
+
+class CustodyChain {
+    CustodyNode* head;
+    CustodyNode* tail;
+
+public:
+    CustodyChain() {
+        head = tail = NULL;
+    }
+
+    void addRecord(string action, string user) {
+        time_t now = time(0);
+        string ts = ctime(&now);
+        CustodyNode* node = new CustodyNode(action, user, ts);
+
+        if (!head) {
+            head = tail = node;
+        } else {
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    void display() {
+        CustodyNode* temp = head;
+        while (temp) {
+            cout << "Action: " << temp->action
+                 << " | User: " << temp->user
+                 << " | Time: " << temp->timestamp;
+            temp = temp->next;
+        }
+    }
+};
