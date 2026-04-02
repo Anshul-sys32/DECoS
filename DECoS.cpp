6c85f7b698bfadeb9644e94968c18a8e3bf73107
Fameless <97400398+Anshul-sys32@users.noreply.github.com>
2026-04-02 08:19:04 +0530

    Initial Project Setup
    setting up Required libraries , Basic hashing function , Global structure foundation

---
+++ DECoS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <ctime>
+using namespace std;
+
+// Simple hash function (DJB2)
+string simpleHash(const string &data) {
+    unsigned long hash = 5381;
+    for (char c : data) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return to_string(hash);
+}
